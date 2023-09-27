5f3d4480485f6f8d20c6162e5d3f2f0c65811077
tobi101 <saibot.nur@gmail.com>
2023-09-27 20:07:43 +0300

    Add CMakeList and main

---
+++ build/CMakeFiles/OpenGLTraining.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLTraining.dir/main.cpp.obj"
+  "CMakeFiles/OpenGLTraining.dir/main.cpp.obj.d"
+  "OpenGLTraining.exe"
+  "OpenGLTraining.exe.manifest"
+  "OpenGLTraining.pdb"
+  "libOpenGLTraining.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLTraining.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/OpenGLTraining.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for OpenGLTraining.
+# This may be replaced when dependencies are built.
